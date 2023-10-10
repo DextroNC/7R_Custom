ff493dd9322ed8d57c2866e372500eae58d81f9d
DextroNC <dextro@email.de>
2023-10-10 17:19:27 +0200

    invisible backpacks

--- 7R_Weapon_Adjustments/CfgVehicles.hpp
+++ 7R_Weapon_Adjustments/CfgVehicles.hpp
@@ -1,4 +1,28 @@
 ﻿class CfgVehicles {
+	class B_AssaultPack_cbr;
+	class B_AssaultPack_invisible: B_AssaultPack_cbr {
+		_generalMacro = "B_AssaultPack_invisible";
+		displayName = "Invisible Assualt Pack";
+		model = "\A3\weapons_f\empty";
+	};
+	class B_Carryall_cbr;
+	class B_Carryall_invisible: B_Carryall_cbr {
+		_generalMacro = "B_Carryall_invisible";
+		displayName = "Invisible Carryall";
+		model = "\A3\weapons_f\empty";
+	};
+	class B_Kitbag_cbr;
+	class B_Kitbag_invisible: B_Kitbag_cbr {
+		_generalMacro = "B_Kitbag_invisible";
+		displayName = "Invisible Kitbag";
+		model = "\A3\weapons_f\empty";
+	};
+	class B_FieldPack_cbr;
+	class B_FieldPack_invisible: B_FieldPack_cbr {
+		_generalMacro = "B_FieldPack_invisible";
+		displayName = "Invisible Field Pack";
+		model = "\A3\weapons_f\empty";
+	};
 	class B_AssaultPack_Base;
 	class rhs_rpg : B_AssaultPack_Base {
 		maximumLoad = 180;
