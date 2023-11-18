212ab93aa069faaaa594209524b38387b1ab1855
DextroNC <dextro@email.de>
2023-11-18 16:32:43 +0100

    wirecutters

--- 7R_Weapon_Adjustments/CfgWeapons.hpp
+++ 7R_Weapon_Adjustments/CfgWeapons.hpp
@@ -554,4 +554,75 @@
 			containerClass = "Supply160";
 		};
 	};
+	class NVGoggles;
+	class USP_GPNVG18: NVGoggles {
+		modelOptics = "";
+		ace_nightvision_bluRadius = 0.26;
+		ace_nightvision_generation = 4;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_quad_4096.paa";
+		ace_nightvision_colorPreset[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+	};
+	class USP_GPNVG18_Green: NVGoggles {
+		modelOptics = "";
+		scope = 2;
+		scopeArsenal=2;
+		displayName = "[USP] L3 GPNVG-18 (BLK) - Green";
+		ace_nightvision_bluRadius = 0.26;
+		ace_nightvision_generation = 4;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_quad_4096.paa";
+		ace_nightvision_colorPreset[] = {0, {0.0, 0.0, 0.0, 0.0}, {1.3, 1.2, 0.0, 0.9}, {6, 1, 1, 0.0}};
+	};
+	class USP_GPNVG18_Green: USP_GPNVG18_TAN {
+		modelOptics = "";
+		displayName = "[USP] L3 GPNVG-18 (TAN) - Green";
+		ace_nightvision_bluRadius = 0.26;
+		ace_nightvision_generation = 4;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_quad_4096.paa";
+		ace_nightvision_colorPreset[] = {0, {0.0, 0.0, 0.0, 0.0}, {1.3, 1.2, 0.0, 0.9}, {6, 1, 1, 0.0}};
+	};
+	class USP_PVS31: NVGoggles {
+		modelOptics = "";
+		ace_nightvision_bluRadius = 0.15;
+		ace_nightvision_generation = 4;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_binos_4096.paa";
+		ace_nightvision_colorPreset[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+	};
+	class USP_NSEAS: NVGoggles {
+		modelOptics = "";
+		ace_nightvision_bluRadius = 0.15;
+		ace_nightvision_generation = 4;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_binos_4096.paa";
+		ace_nightvision_colorPreset[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+	};
+	class USP_PVS14: NVGoggles {
+		modelOptics = "";
+		ace_nightvision_bluRadius = 0.13;
+		ace_nightvision_generation = 3;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_4096.paa";
+		ace_nightvision_colorPreset[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+	};
+	class USP_PVS15: NVGoggles {
+		modelOptics = "";
+		ace_nightvision_bluRadius = 0.15;
+		ace_nightvision_generation = 3;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_binos_4096.paa";
+		ace_nightvision_colorPreset[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+	};
+	class USP_PVS31_MONOL: USP_PVS31 {
+		ace_nightvision_bluRadius = 0.13;
+		ace_nightvision_generation = 4;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_4096.paa";
+		ace_nightvision_colorPreset[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+	};
+	class USP_PVS31_MONOR: USP_PVS31 {
+		ace_nightvision_bluRadius = 0.13;
+		ace_nightvision_generation = 4;
+		ace_nightvision_border = "\z\ace\addons\nightvision\data\nvg_mask_4096.paa";
+		ace_nightvision_colorPreset[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+	};
+	class ACE_ItemCore;
+	class ACE_wirecutter: ACE_ItemCore {
+		displayName = "Breaching Tools";
+		descriptionShort = "Kuts wires and breaches doors";
+	};
 };
